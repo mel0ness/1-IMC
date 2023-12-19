c8ec31ad548e802605958eb85df9cc7604de556c
prof <spasmodic51@gmail.com>
2023-12-19 20:21:35 +0100

    Responsive et JS terminés

--- app.js
+++ app.js
@@ -8,3 +8,80 @@ const BMIData = [
 ];
 
 // IMC = poids en kg / taille² en m
+
+let Poids = null;
+let Taille = null;
+let Resultat = null;
+let TailleM = null;
+
+const InputTaille = document.getElementById("taille");
+const InputPoids = document.getElementById("poids");
+
+InputTaille.addEventListener("change", () => {
+  Taille = InputTaille.value;
+  TailleM = InputTaille.value / 100;
+})
+InputPoids.addEventListener("change", () => {
+  Poids = InputPoids.value;
+})
+
+const Calcul = document.getElementById("calcul");
+const Result = document.getElementById("result");
+const Comment = document.getElementById("comment");
+const Error = document.getElementById("error");
+
+Calcul.addEventListener("click", (e) => {
+  e.preventDefault(); 
+  if(Poids === null) {
+Error.classList.add("error")
+Error.textContent = "Veuillez renseigner votre Poids!"
+Result.textContent = 0;
+Comment.style.color = "black";
+Comment.textContent = "En attente du résultat...";  
+  }
+  else if(Taille === null) {
+    Error.classList.add("error")
+Error.textContent = "Veuillez renseigner votre Taille!"
+Result.textContent = 0;
+Comment.style.color = "black";
+Comment.textContent = "En attente du résultat..."; 
+  }
+  else if(Taille <= 0 || Poids <= 0) {
+    Error.classList.add("error")
+Error.textContent = "Les informations indiquées sont erronées."
+Result.textContent = 0;
+Comment.style.color = "black";
+Comment.textContent = "En attente du résultat..."; 
+  }
+  else if(Taille > 0 && Poids > 0) {
+    Error.classList.remove("error")
+    Error.textContent = ""
+    Resultat = ((Poids / (TailleM * TailleM)).toFixed(1))
+    Result.textContent = Resultat;
+
+    if(BMIData[0].range[0] < Resultat && Resultat <= BMIData[0].range[1]) {
+      Comment.style.color = BMIData[0].color;
+      Comment.textContent = BMIData[0].name;  
+    }
+    else if(BMIData[1].range[0] < Resultat && Resultat <= BMIData[1].range[1]) {
+      Comment.style.color = BMIData[1].color;
+      Comment.textContent = BMIData[1].name; 
+    }
+    else if(BMIData[2].range[0] < Resultat && Resultat <= BMIData[2].range[1]) {
+      Comment.style.color = BMIData[2].color;
+      Comment.textContent = BMIData[2].name;  
+    }
+    else if(BMIData[3].range[0] < Resultat && Resultat <= BMIData[3].range[1]) {
+      Comment.style.color = BMIData[3].color;
+      Comment.textContent = BMIData[3].name;  
+    }
+    else if(BMIData[4].range[0] < Resultat && Resultat <= BMIData[4].range[1]) {
+      Comment.style.color = BMIData[4].color;
+      Comment.textContent = BMIData[4].name;  
+    }
+    else if(BMIData[5].range < Resultat) {
+      Comment.style.color = BMIData[5].color;
+      Comment.textContent = BMIData[5].name;  
+    }
+  }
+})
